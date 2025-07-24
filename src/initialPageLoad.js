c450c73d7d5bb76f97f7360af752933210969ea5
varbris <92345143+Varbris@users.noreply.github.com>
2025-07-24 15:37:29 +0700

    WIP: content style

--- src/initialPageLoad.js
+++ src/initialPageLoad.js
@@ -1,11 +1,17 @@
 export function pageLoad() {
   const content = document.getElementById("content");
-  const container = document.createElement("div");
+  const containerHeader = document.createElement("div");
+  const introductionContainer = document.createElement("div");
+  introductionContainer.classList.add("content-article");
+  containerHeader.classList.add("content-header");
   const text = document.createElement("h1");
   const p = document.createElement("p");
   content.innerText = "";
+  p.innerText = "The best pizza in town";
+
   text.innerText = "PIZZARIA MAMMAMIA";
-  container.appendChild(text);
-  container.appendChild(p);
-  content.appendChild(container);
+  containerHeader.appendChild(text);
+  introductionContainer.appendChild(p);
+  content.appendChild(containerHeader);
+  content.appendChild(introductionContainer);
 }
