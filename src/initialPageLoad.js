b93ef04d16f527a7c982a08445a60046ea75924f
varbris <92345143+Varbris@users.noreply.github.com>
2025-07-25 14:38:19 +0700

    style: fix landing page image

--- src/initialPageLoad.js
+++ src/initialPageLoad.js
@@ -7,11 +7,26 @@ export function pageLoad() {
   myImage.src = pizzaImage;
   contentContainer.innerHTML = `
     <div class="content-header">
-        <h1>PIZZARIA MAMMAMIA</h1>
-    </div>
-    <div class="content-article">
-      <img src="${myImage.src}" alt="" class="pizaria-img">
-      <p>The best pizza in town</p>
-    </div>
+  <h1>PIZZARIA MAMMAMIA</h1>
+</div>
+<div class="content-article">
+  <ul class="content-article-list">
+    <li>
+      <div class="article1 article-border ">
+        <img src="${myImage.src}" alt="" class="pizaria-img" />
+        <p class="motto">The best pizza in town</p>
+      </div>
+    </li>
+    <li>
+      <div class="article2 article-border ">
+        <ul class="open-schedule">
+          <li>Monday - Friday: 9:00 AM - 6:00 PM</li>
+          <li>Saturday: 10:00 AM - 4:00 PM</li>
+          <li>Sunday: Closed</li>
+        </ul>
+      </div>
+    </li>
+  </ul>
+</div>
   `;
 }
